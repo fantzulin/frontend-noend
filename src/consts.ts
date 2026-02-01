64283e766b10e717753e44088c08f1a559a24997
Chris.Fan <fanchi0917@gmail.com>
2026-02-02 00:47:37 +0800

    refactor: update site branding, enhance footer and header links, and improve blog layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Frontend Noend';
+export const SITE_DESCRIPTION = 'I am Chris, a frontend developer. This blog is a collection of my thoughts and experiences as a developer.';
