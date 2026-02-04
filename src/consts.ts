b376a8fa73d904ebc6298bfb405d765914a538c4
Chris.Fan <fanchi0917@gmail.com>
2026-02-04 18:31:46 +0800

    feat: update site description, add new blog post with images, and enhance styling for headings

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Frontend Noend';
-export const SITE_DESCRIPTION = 'I am Chris, a frontend developer. This blog is a collection of my thoughts and experiences as a developer.';
+export const SITE_DESCRIPTION = 'I am Chris, a frontend developer. This blog is a collection of my thoughts and experiences as a developer. May the frontend be with you.';
